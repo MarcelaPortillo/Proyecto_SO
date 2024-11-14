3f87ff4718cbb3f03f612f1241f913ed17abf904
Marcela Portillo <00079419@uca.edu.sv>
2024-11-14 15:04:57 -0600

    Cambiando entorno base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/MarcelaPortillo/Proyecto_SO',
+  base: '/Proyecto_SO/',
 })
